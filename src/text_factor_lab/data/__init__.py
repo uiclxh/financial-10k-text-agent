02e2470d360015a30e44cf52b28360fc940826f9
Tom Lin <linxihang0810@gmail.com>
2026-05-19 22:16:12 +0800

    Export price data helpers

--- src/text_factor_lab/data/__init__.py
+++ src/text_factor_lab/data/__init__.py
@@ -1,5 +1,11 @@
 """Data acquisition and manifest building."""
 
+from text_factor_lab.data.prices import (
+    PriceDataError,
+    PricePanel,
+    build_price_panel,
+    load_price_panel_csv,
+)
 from text_factor_lab.data.sec_edgar import (
     build_archive_document_url,
     build_document_manifest_from_submissions,
@@ -19,13 +25,17 @@ from text_factor_lab.data.universe import (
 __all__ = [
     "build_archive_document_url",
     "build_document_manifest_from_submissions",
+    "build_price_panel",
     "build_universe_quality_report",
     "build_submissions_url",
     "extract_annual_filings",
     "filing_row_to_manifest_record",
     "load_and_report_universe",
+    "load_price_panel_csv",
     "load_universe_manifest",
     "normalize_cik",
     "parse_sec_acceptance_datetime",
+    "PriceDataError",
+    "PricePanel",
     "write_manifest_jsonl",
 ]
