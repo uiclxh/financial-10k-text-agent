36db82aa4d7f2155633794a7f56c02a63768e3a5
Tom Lin <linxihang0810@gmail.com>
2026-05-19 21:58:55 +0800

    Export SEC 10-K parser helpers

--- src/text_factor_lab/parsing/__init__.py
+++ src/text_factor_lab/parsing/__init__.py
@@ -1,1 +1,23 @@
-"""SEC filing parsing and text section extraction."""
+"""SEC filing parsing utilities."""
+
+from text_factor_lab.parsing.sec_10k_sections import (
+    SEC_10K_SECTION_PARSER_VERSION,
+    SectionExtractionResult,
+    find_item_heading_candidates,
+    normalize_sec_document_text,
+    parse_sec_10k_sections,
+    read_parsed_sections_jsonl,
+    should_parse_sec_filing,
+    write_section_artifacts,
+)
+
+__all__ = [
+    "SEC_10K_SECTION_PARSER_VERSION",
+    "SectionExtractionResult",
+    "find_item_heading_candidates",
+    "normalize_sec_document_text",
+    "parse_sec_10k_sections",
+    "read_parsed_sections_jsonl",
+    "should_parse_sec_filing",
+    "write_section_artifacts",
+]
