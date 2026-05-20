8e1fe04a43e11c4a0178d37d9b796578bdc6d94f
Tom Lin <linxihang0810@gmail.com>
2026-05-20 16:21:53 +0800

    Export evaluation backtest helpers

--- src/text_factor_lab/backtest/__init__.py
+++ src/text_factor_lab/backtest/__init__.py
@@ -1,1 +1,27 @@
 """Factor portfolio construction and backtesting."""
+
+from text_factor_lab.backtest.evaluation import (
+    BACKTEST_VERSION,
+    EvaluationBuildResult,
+    ScoredPrediction,
+    build_evaluation_artifacts,
+    newey_west_t_stat,
+    pearson_ic,
+    read_labels_jsonl,
+    read_predictions_jsonl,
+    write_backtest_results_json,
+    write_evaluation_metrics_json,
+)
+
+__all__ = [
+    "BACKTEST_VERSION",
+    "EvaluationBuildResult",
+    "ScoredPrediction",
+    "build_evaluation_artifacts",
+    "newey_west_t_stat",
+    "pearson_ic",
+    "read_labels_jsonl",
+    "read_predictions_jsonl",
+    "write_backtest_results_json",
+    "write_evaluation_metrics_json",
+]
