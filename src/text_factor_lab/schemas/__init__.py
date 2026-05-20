34aa54c675efe6f31a93386db605406a49902732
Tom Lin <linxihang0810@gmail.com>
2026-05-20 15:50:43 +0800

    Export feature manifest schema

--- src/text_factor_lab/schemas/__init__.py
+++ src/text_factor_lab/schemas/__init__.py
@@ -2,7 +2,7 @@
 
 from text_factor_lab.schemas.config import ExperimentConfig, load_experiment_config
 from text_factor_lab.schemas.document_manifest import DocumentManifestRecord
-from text_factor_lab.schemas.features import FeatureRecord
+from text_factor_lab.schemas.features import FeatureManifestRecord, FeatureRecord
 from text_factor_lab.schemas.labels import LabelRecord
 from text_factor_lab.schemas.model_manifest import ModelManifestRecord
 from text_factor_lab.schemas.parsed_sections import ParsedSectionRecord, ParsingQualityReport
@@ -14,6 +14,7 @@ from text_factor_lab.schemas.universe import UniverseQualityReport, UniverseReco
 __all__ = [
     "DocumentManifestRecord",
     "ExperimentConfig",
+    "FeatureManifestRecord",
     "FeatureRecord",
     "LabelRecord",
     "ModelManifestRecord",
