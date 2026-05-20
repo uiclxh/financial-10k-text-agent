8dbc8c40b8d8defd099c496e9a22b619dbbb9310
Tom Lin <linxihang0810@gmail.com>
2026-05-20 16:09:22 +0800

    Export tuning log schema

--- src/text_factor_lab/schemas/__init__.py
+++ src/text_factor_lab/schemas/__init__.py
@@ -4,7 +4,7 @@ from text_factor_lab.schemas.config import ExperimentConfig, load_experiment_con
 from text_factor_lab.schemas.document_manifest import DocumentManifestRecord
 from text_factor_lab.schemas.features import FeatureManifestRecord, FeatureRecord
 from text_factor_lab.schemas.labels import LabelRecord
-from text_factor_lab.schemas.model_manifest import ModelManifestRecord
+from text_factor_lab.schemas.model_manifest import ModelManifestRecord, TuningLogRecord
 from text_factor_lab.schemas.parsed_sections import ParsedSectionRecord, ParsingQualityReport
 from text_factor_lab.schemas.predictions import PredictionRecord
 from text_factor_lab.schemas.run_status import RunStatusRecord
@@ -24,6 +24,7 @@ __all__ = [
     "RunStatusRecord",
     "SplitAssignmentRecord",
     "SplitLeakageRecord",
+    "TuningLogRecord",
     "UniverseQualityReport",
     "UniverseRecord",
     "load_experiment_config",
