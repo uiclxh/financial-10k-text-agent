88a2892e30fc8d1575dedcd64b96968bf19e00eb
Tom Lin <linxihang0810@gmail.com>
2026-05-20 15:08:41 +0800

    Export text feature builders

--- src/text_factor_lab/features/__init__.py
+++ src/text_factor_lab/features/__init__.py
@@ -1,1 +1,37 @@
 """Text feature construction."""
+
+from text_factor_lab.features.text import (
+    DEFAULT_FINANCIAL_DICTIONARIES,
+    DICTIONARY_FEATURE_VERSION,
+    TFIDF_FEATURE_VERSION,
+    DocumentText,
+    FeatureBuildResult,
+    build_dictionary_tone_features,
+    build_tfidf_features,
+    document_id_from_label_id,
+    load_document_texts,
+    read_document_manifest_jsonl,
+    read_parsed_sections_jsonl,
+    read_split_assignments_jsonl,
+    tokenize,
+    write_features_jsonl,
+    write_vocabulary_json,
+)
+
+__all__ = [
+    "DEFAULT_FINANCIAL_DICTIONARIES",
+    "DICTIONARY_FEATURE_VERSION",
+    "TFIDF_FEATURE_VERSION",
+    "DocumentText",
+    "FeatureBuildResult",
+    "build_dictionary_tone_features",
+    "build_tfidf_features",
+    "document_id_from_label_id",
+    "load_document_texts",
+    "read_document_manifest_jsonl",
+    "read_parsed_sections_jsonl",
+    "read_split_assignments_jsonl",
+    "tokenize",
+    "write_features_jsonl",
+    "write_vocabulary_json",
+]
