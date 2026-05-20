9bcfd8787768fa50f52a0eb69ed883e180065abe
Tom Lin <linxihang0810@gmail.com>
2026-05-20 16:08:21 +0800

    Export model training helpers

--- src/text_factor_lab/models/__init__.py
+++ src/text_factor_lab/models/__init__.py
@@ -1,1 +1,29 @@
 """Model training and prediction output."""
+
+from text_factor_lab.models.training import (
+    DEFAULT_RIDGE_ALPHA_GRID,
+    MODEL_TRAINING_VERSION,
+    ModelBuildResult,
+    build_model_artifacts,
+    rank_ic,
+    read_features_jsonl,
+    read_labels_jsonl,
+    read_split_assignments_jsonl,
+    write_model_manifest_json,
+    write_predictions_jsonl,
+    write_tuning_log_json,
+)
+
+__all__ = [
+    "DEFAULT_RIDGE_ALPHA_GRID",
+    "MODEL_TRAINING_VERSION",
+    "ModelBuildResult",
+    "build_model_artifacts",
+    "rank_ic",
+    "read_features_jsonl",
+    "read_labels_jsonl",
+    "read_split_assignments_jsonl",
+    "write_model_manifest_json",
+    "write_predictions_jsonl",
+    "write_tuning_log_json",
+]
